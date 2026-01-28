383ba5662d48eb9067200d15f6b3c2d2889c7537
Danni Kadoshi <dannykadoshi@me.com>
2026-01-28 15:49:07 +0000

    Remove redundant discount code table creation migration

--- checkout/migrations/0009_discountcode_banner_message.py
+++ checkout/migrations/0009_discountcode_banner_message.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('checkout', '0008_fix_discountcode_table'),
+        ('checkout', '0007_discountcode_order_discount_amount_and_more'),
     ]
 
     operations = [
